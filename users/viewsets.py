e98ba32c41c2f7dcdfc9c6a5d4a19cfe10c21c92
Iuri Guilherme Bogar Portilho <79106998+ibogar@users.noreply.github.com>
2026-05-28 15:24:51 -0300

    Update users viewsets.py - add auth

--- users/viewsets.py
+++ users/viewsets.py
@@ -1,10 +1,25 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import ( AllowAny, IsAuthenticated )
 
 from .models import User
 from .serializers import UserSerializer
+from .permissions import IsOwner
 
 
 class UserViewSet(ModelViewSet):
 
     queryset = User.objects.all().order_by("id")
     serializer_class = UserSerializer
+
+    def get_permissions(self):
+
+        if self.action == 'create':
+            permission_classes = [AllowAny]
+
+        elif self.action in ['update', 'partial_update', 'destroy']:
+            permission_classes = [IsOwner]
+
+        else:
+            permission_classes = [IsAuthenticated]
+
+        return [permission() for permission in permission_classes]
