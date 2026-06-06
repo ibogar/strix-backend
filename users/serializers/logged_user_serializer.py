0683d75caa8e3cb513843dd5a6bb2bb6c2d822d5
Iuri Guilherme Bogar Portilho <iuribogar@hotmail.com>
2026-06-06 17:24:46 -0300

    update serializers - simpler logged_user, and add follow count to main serializer

--- users/serializers/logged_user_serializer.py
+++ users/serializers/logged_user_serializer.py
@@ -7,17 +7,9 @@ from users.models import User
 
 class LoggedUserSerializer(serializers.ModelSerializer):
     
-    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
-
     class Meta:
         model = User
         fields = [
-            'email',
-            'full_name',
+            'id',
             'username',
-            'bio',
-            'profile_picture',
-            'password',
-            'following',
-            'followers'
         ]
--- users/serializers/user_serializer.py
+++ users/serializers/user_serializer.py
@@ -9,6 +9,8 @@ class UserSerializer(serializers.ModelSerializer):
     
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
     email = serializers.CharField(write_only=True, required=True)
+    following_count = serializers.SerializerMethodField()
+    followers_count = serializers.SerializerMethodField()
 
     class Meta:
         model = User
@@ -19,7 +21,15 @@ class UserSerializer(serializers.ModelSerializer):
             'password',
             'bio',
             'profile_picture',
+            'following_count',
+            'followers_count',
         ]
+
+    def get_followers_count(self, obj):
+        return obj.followers.count()
+
+    def get_following_count(self, obj):
+        return obj.following.count()
 
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
