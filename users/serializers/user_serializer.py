d6ac2c8bbf8f5e7d93640e0820b328c8d0865ded
Iuri Guilherme Bogar Portilho <iuribogar@hotmail.com>
2026-06-08 06:56:54 -0300

    update user_serializer

--- users/serializers/user_serializer.py
+++ users/serializers/user_serializer.py
@@ -15,6 +15,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = [
+            'id',
             'email',
             'full_name',
             'username',
